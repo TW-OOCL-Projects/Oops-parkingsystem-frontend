7624e78cf0b71d5f5a8e3d67ca6c274f02062afa
louis <louis.z.j.li@oocl.com>
2018-07-31 15:15:49 +0800

    [louis] refacter the routers and tabbars

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'antd/dist/antd.css'
-import HomePage from "./compoments/home"
-import employeeMangement from "./compoments/employee-mangement";
-import ParkingLotMangement from "./compoments/parkingLot-management";
-import Dashboarsh from "./compoments/dashboarsh";
-import OrderManagement from "./compoments/order-management"
-import ParkingBoy from "./compoments/parking-boy"
-
-import {Route,Link } from "react-router-dom";
-import { Layout, Menu, Icon } from 'antd';
-const { Header, Sider, Content } = Layout;
+import 'antd/dist/antd.css';
+import Router from "./Router";
+import Tabbars from "./compoments/tabbars";
+import { Layout,Icon } from 'antd';
+const { Header,Content } = Layout;
 // import API from "./API/Example"
 class App extends Component {
   constructor(props) {
@@ -30,47 +24,7 @@ class App extends Component {
   render() {
     return (
       <Layout className="layout">
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collapsed}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-           
-              <Menu.Item key="1">
-              <Link to="/employeeMangment">
-                <Icon type="user" />
-                <span>员工管理</span>
-                </Link>
-              </Menu.Item>
-            
-            <Menu.Item key="2">
-            <Link to="/parkingLotMangement">
-              <Icon type="video-camera" />
-              <span>停车场管理</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-            <Link to="/parkingBoy">
-              <Icon type="upload" />
-              <span>停车员管理</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="5">
-            <Link to="/dashboarsh">
-              <Icon type="codepen-circle" />
-              <span>停车场Dashboarsh</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="6">
-            <Link to="/orderManagement">
-              <Icon type="dropbox" />
-              <span>订单管理</span>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+       <Tabbars collapsed = {this.state.collapsed}/>
         <Layout style={{ Width: "100%" }}>
           <Header style={{ background: '#fff', padding: 0 }}>
             <Icon
@@ -81,12 +35,7 @@ class App extends Component {
           </Header>
           <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280, minWidth: "100%" }}>
               <div>
-                <Route exact path="/home" component={HomePage} />
-                <Route  path="/employeeMangment" component={employeeMangement} />
-                <Route  path="/parkingLotMangement" component={ParkingLotMangement} />
-                <Route  path="/dashboarsh" component={Dashboarsh} />
-                <Route  path="/orderManagement" component={OrderManagement} />
-                <Route  path="/parkingBoy" component={ParkingBoy} />
+              <Router/>
               </div >
           </Content>
         </Layout>
