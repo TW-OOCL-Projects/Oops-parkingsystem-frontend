dcbaa9012dc8599538abb9ae21f62d20cb035b2e
louis <louis.z.j.li@oocl.com>
2018-07-31 15:38:30 +0800

    [louis] login and logout head

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import 'antd/dist/antd.css';
 import Router from "./Router";
 import Tabbars from "./compoments/tabbars";
-import { Layout,Icon } from 'antd';
-const { Header,Content } = Layout;
+import { Avatar, Layout, Icon, Menu, Dropdown, message } from 'antd';
+const { Header, Content } = Layout;
 // import API from "./API/Example"
 class App extends Component {
   constructor(props) {
@@ -20,23 +20,37 @@ class App extends Component {
   }
   componentDidMount() {
   }
-
+  
   render() {
+    const onClick = function ({ key }) {
+      message.info(`Click on item ${key}`);
+    };
+    const menu = (
+      <Menu onClick={onClick}>
+        <Menu.Item key="1">1st menu item</Menu.Item>
+        <Menu.Item key="2">2nd memu item</Menu.Item>
+        <Menu.Item key="3">3rd menu item</Menu.Item>
+      </Menu>
+    );
     return (
       <Layout className="layout">
-       <Tabbars collapsed = {this.state.collapsed}/>
+        <Tabbars collapsed={this.state.collapsed} />
         <Layout style={{ Width: "100%" }}>
-          <Header style={{ background: '#fff', padding: 0 }}>
+          <Header style={{ background: '#fff', padding: 0 }} className="Header">
             <Icon
               className="trigger"
               type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
+              <Dropdown overlay={menu} className="Dropdown">
+                <Avatar size="large" icon="user"  />
+              </Dropdown>
+           
           </Header>
           <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280, minWidth: "100%" }}>
-              <div>
-              <Router/>
-              </div >
+            <div>
+              <Router />
+            </div >
           </Content>
         </Layout>
       </Layout>
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux'
 import reducers from './reducers'
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom";
 const store = createStore(reducers)
 const rootEl = document.getElementById('root')
 
 ReactDOM.render(
     <Provider store={store}>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>,
     rootEl
 )
