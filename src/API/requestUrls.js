95702d40c5520d672ad70e041e7c6b2065b1f1cd
曹鹤丹 <amelia_cao@caohedandeMacBook-Pro.local>
2018-08-03 03:26:22 +0800

    [amelia]combine_select

--- src/API/requestUrls.js
+++ src/API/requestUrls.js
@@ -1,5 +1,7 @@
 // const globalUrl = "https://parkinglot-backend.herokuapp.com/api/v1/" //dev
-const globalUrl = "http://10.222.232.147:8080/api/v1/" //test
+// const globalUrl = "http://10.222.232.147:8080/api/v1/" //test
+const globalUrl = "http://localhost:8080/api/v1/"
+
 export default {
     "login":globalUrl + "login",
     "employees":globalUrl + "users",
