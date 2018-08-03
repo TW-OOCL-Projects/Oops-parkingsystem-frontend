3a52d0972af837fc051f9eb70d30b527102fee68
louis <louis.z.j.li@oocl.com>
2018-08-03 22:05:40 +0800

    [louis] fix the conflict

--- src/API/requestUrls.js
+++ src/API/requestUrls.js
@@ -1,6 +1,6 @@
 // const globalUrl = "https://parkinglot-backend.herokuapp.com/api/v1/" //dev
 const globalUrl = "http://10.222.232.147:8080/api/v1/" //test
- const globalUrl = "http://10.222.232.148:8080/api/v1/" // carol test
+//  const globalUrl = "http://10.222.232.148:8080/api/v1/" // carol test
 
 export default {
     "login":globalUrl + "login",
