be0c6e790190d0e4b62d76cb332a52a316fa2deb
beeMonkey <675383749@qq.com>
2018-08-03 13:53:52 +0800

    [Allen]fix the confict in dashboard

--- src/compoments/dashboarsh.js
+++ src/compoments/dashboarsh.js
@@ -16,12 +16,12 @@ class Dashboarsh extends Component {
         console.log(items)
         return (
             <div >
-             <Row type="flex" justify="start">
+             <Row type="flex" justify="space-around">
                  {items.map(item=>{
                     //  return <li>{item.id}{item.name}{item.size}{item.countOfCars}</li>
-                    console.log(item)
-                    return <Col span={8} style={{marginBottom:"15px"}}>   
-                            <div style={{width:"350px"}}>
+                    return <Col xs={24} sm={10} md={7} lg={7} xl={5} style={{marginBottom:"15px"}}>   
+                            {/* <div style={{width:"350px"}}> */}
+                            <div>
                                 <Collapse defaultActiveKey={['1']} >
                                 <Panel showArrow={false} header={item.name} key="1">  
                                 <Row type="flex" justify="space-around" align="middle">
